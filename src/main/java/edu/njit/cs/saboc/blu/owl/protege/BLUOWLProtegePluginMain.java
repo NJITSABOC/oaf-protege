ea172722c189d07a6aacb1659afc4805ff2ecba1
snoop_000 <snoop_000@10.0.0.220>
2015-10-15 20:54:28 -0400

    Modifications to use new UI

--- src/main/java/edu/njit/cs/saboc/blu/owl/protege/BLUOWLProtegePluginMain.java
+++ src/main/java/edu/njit/cs/saboc/blu/owl/protege/BLUOWLProtegePluginMain.java
@@ -15,13 +15,13 @@ import edu.njit.cs.saboc.blu.owl.utils.owlproperties.*;
 import edu.njit.cs.saboc.blu.owl.abn.pareataxonomy.*;
 import edu.njit.cs.saboc.blu.owl.gui.abnselection.OWLDisplayFrameListener;
 
-import edu.njit.cs.saboc.blu.owl.gui.dialogs.listeners.OWLPAreaDetailsActionAdapter;
-import edu.njit.cs.saboc.blu.owl.gui.graphframe.*;
+import edu.njit.cs.saboc.blu.owl.gui.graphframe.OWLInternalPAreaTaxonomyGraphFrame;
 import edu.njit.cs.saboc.blu.owl.gui.nat.OWLInternalConceptBrowserFrame;
 import edu.njit.cs.saboc.blu.owl.gui.nat.OWLNATAdjustableLayout;
 import edu.njit.cs.saboc.blu.owl.protege.nat.UpdatingOWLBrowserDataSource;
 import edu.njit.cs.saboc.nat.generic.gui.panels.FocusConceptPanel;
 import java.awt.GraphicsEnvironment;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
@@ -42,7 +42,7 @@ public class BLUOWLProtegePluginMain extends AbstractOWLViewComponent {
 
     private class ProtegeOWLTaxonomyPanel extends JPanel {
 
-        public ProtegeOWLTaxonomyPanel(OWLInternalGraphFrame graphFrame) {
+        public ProtegeOWLTaxonomyPanel(OWLInternalPAreaTaxonomyGraphFrame graphFrame) {
             super(new BorderLayout());
 
             this.add(graphFrame.getContentPane(), BorderLayout.CENTER);
@@ -50,6 +50,25 @@ public class BLUOWLProtegePluginMain extends AbstractOWLViewComponent {
     }
 
     private JTabbedPane tabbedPane = new JTabbedPane();
+    
+    private final OWLDisplayFrameListener displayListener = new OWLDisplayFrameListener(getMyFrame()) {
+        public void displayFrame(final JInternalFrame internalFrame) {
+            Container contentPane = internalFrame.getContentPane();
+
+            JFrame contentFrame = new JFrame();
+
+            contentFrame.setTitle("Biomedical Layout Utility for OWL (BLUOWL) by SABOC");
+
+            contentFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+            contentFrame.setBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
+
+            contentFrame.add(contentPane);
+
+            contentFrame.setVisible(true);
+        }
+    };
+         
 
     protected void initialiseOWLView() throws Exception {
         setLayout(new BorderLayout());
@@ -58,6 +77,8 @@ public class BLUOWLProtegePluginMain extends AbstractOWLViewComponent {
 
         OWLModelManagerListener modelListener = new OWLModelManagerListener() {
             public void handleChange(OWLModelManagerChangeEvent event) {
+                log.debug("In ontology model manager change listener...");
+                
                 switch (event.getType()) {
                     case ACTIVE_ONTOLOGY_CHANGED:
                         initializeBLUOWL();
@@ -69,6 +90,8 @@ public class BLUOWLProtegePluginMain extends AbstractOWLViewComponent {
                     case REASONER_CHANGED:
                     case ABOUT_TO_CLASSIFY:
                     case ONTOLOGY_CLASSIFIED:
+                        
+                        
                     case ONTOLOGY_CREATED:
                     case ONTOLOGY_LOADED:
                     case ONTOLOGY_RELOADED:
@@ -78,85 +101,132 @@ public class BLUOWLProtegePluginMain extends AbstractOWLViewComponent {
                 }
             }
         };
-
         
+        OWLOntologyChangeListener changeListener = new OWLOntologyChangeListener() {
+            public void ontologiesChanged(List<? extends OWLOntologyChange> changes) {
+
+                changes.forEach((OWLOntologyChange change) -> {
+                    OWLOntologyChangeData changeData = change.getChangeData();
+                    OWLOntologyChangeRecord changeRecord = change.getChangeRecord();
+
+                    if (change.isAddAxiom()) {
+                        AddAxiom addAxiom = (AddAxiom) change;
+
+                        AddAxiomData addAxiomData = addAxiom.getChangeData();
+
+                    } else if (change.isAxiomChange()) {
+                        OWLAxiomChange changeAxiom = (OWLAxiomChange) change;
+
+                    } else if (change.isImportChange()) {
+                        ImportChange importChange = (ImportChange) change;
+
+                    } else if (change.isRemoveAxiom()) {
+                        RemoveAxiom removeAxiom = (RemoveAxiom) change;
+
+                    } else {
+
+                    }
+                });
+                
+                log.debug("In ontology change listener...");
+
+                dataSource.update(conceptBrowserFrame.getBrowser().getFocusConcept());
+
+                Thread taxonomyUpdateThread = new Thread(() -> {
+                    OWLOntologyManager ontologyManager = getOWLModelManager().getOWLOntologyManager();
+
+                    OWLOntology ontology = getOWLModelManager().getActiveOntology();
+
+                    BLUOntologyDataManager loader = new BLUOntologyDataManager(ontologyManager, null, ontology.getOntologyID().toString(), ontology);
+
+                    OWLPAreaTaxonomy currentTaxonomy = (OWLPAreaTaxonomy)graphFrame.getGraph().getAbstractionNetwork();
+                    
+                    HashSet<PropertyUsageType> usages = currentTaxonomy.getPropertyTypes();
+
+                    OWLPAreaTaxonomy newTaxonomy = loader.createCompleteOntologyTaxonomy(usages);
+
+                    if(!currentTaxonomy.getRootGroup().getRootAsOWLClass().equals(newTaxonomy.getRootGroup().getRootAsOWLClass())) {
+                        log.debug("CREATING SUBTAXONOMY...");
+                        newTaxonomy = newTaxonomy.getRootSubtaxonomy(currentTaxonomy.getRootGroup());
+                    }
+                    
+                    if(currentTaxonomy.isReduced()) {
+                        ArrayList<OWLPArea> pareas = new ArrayList<>(currentTaxonomy.getGroupHierarchy().getNodesInHierarchy());
+                        
+                        int smallestSize = pareas.get(0).getConceptCount();
+                        
+                        for(int c = 0; c < pareas.size(); c++) {
+                            if(pareas.get(c).getConceptCount() < smallestSize) {
+                                smallestSize = pareas.get(c).getConceptCount();
+                            }
+                        }
+                        
+                        newTaxonomy = newTaxonomy.getReduced(smallestSize);
+                    }
+                    
+                    graphFrame.replaceInternalFrameDataWith(newTaxonomy, ontology);
+                });
+                
+                taxonomyUpdateThread.start();
+            }
+        };
+                
+        modelManager.addOntologyChangeListener(changeListener);
         modelManager.addListener(modelListener);
+
+        graphFrame = new OWLInternalPAreaTaxonomyGraphFrame(getMyFrame(), displayListener);              
+                            
+        tabbedPane.add(new ProtegeOWLTaxonomyPanel(graphFrame), "OWL Partial-area Taxonomies");
         
         this.add(tabbedPane, BorderLayout.CENTER);
-
+        
         initializeBLUOWL();
 
         log.info("BLUOWL STATUS: Initialized");
     }
+    
+    private OWLInternalPAreaTaxonomyGraphFrame graphFrame;
 
+    private OWLInternalConceptBrowserFrame conceptBrowserFrame;
+    
+    private UpdatingOWLBrowserDataSource dataSource;
+    
     private void initializeBLUOWL() {
-        tabbedPane.removeAll();
-
         OWLOntologyManager ontologyManager = getOWLModelManager().getOWLOntologyManager();
 
         OWLOntology ontology = getOWLModelManager().getActiveOntology();
 
         BLUOntologyDataManager loader = new BLUOntologyDataManager(ontologyManager, null, ontology.getOntologyID().toString(), ontology);
 
-        log.info(loader.getOntologyRoots());
-
         HashSet<PropertyUsageType> usages = new HashSet<PropertyUsageType>();
-
-        usages.add(PropertyUsageType.OPExplicitDomain);
+        
+        if(loader.getOntologyMetrics().totalOPWithDomainCount > 0) {
+            usages.add(PropertyUsageType.OPExplicitDomain);
+        } else if(loader.getOntologyMetrics().totalOPWithRestrictionCount > 1) {
+            usages.add(PropertyUsageType.OPRestriction);
+        } else {
+            // TODO: Figure out whats really available...
+            usages.add(PropertyUsageType.OPExplicitDomain);
+        }
 
         OWLPAreaTaxonomy taxonomy = loader.createCompleteOntologyTaxonomy(usages);
-
-        log.info("BLUOWL STATUS: Created initial taxonomy");
-
-        OWLInternalGraphFrame graphFrame = new OWLInternalGraphFrame(getMyFrame(), taxonomy, true, false, ontology);
-
-        graphFrame.addDialogActionListener(new OWLPAreaDetailsActionAdapter() {
-            public void propertyDoubleClicked(OWLProperty property) {
-                OWLWorkspace workspace = getOWLWorkspace();
-
-                workspace.getOWLSelectionModel().setSelectedEntity(property);
-
-                workspace.displayOWLEntity(property);
-            }
-
-            public void classDoubleClicked(OWLClass cls) {
-                OWLWorkspace workspace = getOWLWorkspace();
-
-                workspace.getOWLSelectionModel().setSelectedEntity(cls);
-                workspace.displayOWLEntity(cls);
-            }
-        });
-
-        tabbedPane.add(new ProtegeOWLTaxonomyPanel(graphFrame), "Partial-area Taxonomies");
         
-        UpdatingOWLBrowserDataSource dataSource = new UpdatingOWLBrowserDataSource(ontologyManager, ontology);
+        graphFrame.replaceInternalFrameDataWith(taxonomy, ontology);
+        
+        dataSource = new UpdatingOWLBrowserDataSource(ontologyManager, ontology);
+        
+        if(tabbedPane.getTabCount() > 1) {
+            tabbedPane.remove(1);
+        }
+        
+        conceptBrowserFrame = new OWLInternalConceptBrowserFrame(getMyFrame(), dataSource, displayListener);
 
-        OWLInternalConceptBrowserFrame browserFrame = new OWLInternalConceptBrowserFrame(getMyFrame(), dataSource,
-                new OWLDisplayFrameListener(getMyFrame()) {
-                    public void displayFrame(final JInternalFrame internalFrame) {
-                        Container contentPane = internalFrame.getContentPane();
-                        
-                        JFrame contentFrame = new JFrame();
-
-                        contentFrame.setTitle("Biomedical Layout Utility for OWL (BLUOWL) by SABOC");
-
-                        contentFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-                        contentFrame.setBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
-                        
-                        contentFrame.add(contentPane);
-                        
-                        contentFrame.setVisible(true);
-                    }
-                }
-        );
-
-        final FocusConceptPanel<OWLClass> fcPanel = ((OWLNATAdjustableLayout) browserFrame.getBrowser().getNATLayout()).getFocusConceptPanel();
+        final FocusConceptPanel<OWLClass> fcPanel = ((OWLNATAdjustableLayout) conceptBrowserFrame.getBrowser().getNATLayout()).getFocusConceptPanel();
 
         JButton editButton = new JButton("Edit Class");
 
         editButton.addActionListener(e -> {
-            OWLClass cls = (OWLClass) browserFrame.getBrowser().getFocusConcept().getConcept();
+            OWLClass cls = (OWLClass) conceptBrowserFrame.getBrowser().getFocusConcept().getConcept();
 
             OWLWorkspace workspace = getOWLWorkspace();
 
@@ -164,48 +234,11 @@ public class BLUOWLProtegePluginMain extends AbstractOWLViewComponent {
             workspace.displayOWLEntity(cls);
         });
         
-        OWLOntologyChangeListener changeListener = new OWLOntologyChangeListener() {
-            public void ontologiesChanged(List<? extends OWLOntologyChange> changes) {
-                
-                changes.forEach((OWLOntologyChange change) -> {
-                    OWLOntologyChangeData changeData = change.getChangeData();
-                    OWLOntologyChangeRecord changeRecord = change.getChangeRecord();
-                                       
-                    if(change.isAddAxiom()) {
-                        AddAxiom addAxiom = (AddAxiom)change;
-                        
-                        AddAxiomData addAxiomData = addAxiom.getChangeData();
-                        
-                        
-                    } else if(change.isAxiomChange()) {
-                        OWLAxiomChange changeAxiom = (OWLAxiomChange)change;
-                        
-                        
-                    } else if(change.isImportChange()) {
-                        ImportChange importChange = (ImportChange)change;
-                        
-                        
-                    } else if(change.isRemoveAxiom()) {
-                        RemoveAxiom removeAxiom = (RemoveAxiom)change;
-                        
-                    } else {
-                        
-                    }
-                });
-                
-                dataSource.update(browserFrame.getBrowser().getFocusConcept());
-            }
-        };
-        
-        OWLModelManager modelManager = getOWLModelManager();
-        
-        modelManager.addOntologyChangeListener(changeListener);
-
         fcPanel.addOptionButton(editButton);
-
-        tabbedPane.add(browserFrame.getContentPane(), "OWL Neighborhood Auditing Tool (OWL NAT)");
+        
+        tabbedPane.add(conceptBrowserFrame.getContentPane(), "OWL Neighborhood Auditing Tool (OWL NAT)");
     }
-
+   
     private JFrame getMyFrame() {
 
         Container cont = this.getParent();
